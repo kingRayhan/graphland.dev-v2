5d338328805162628465abf4002f4f513b1f68a8
kingrayhan <rayhan095@gmail.com>
2025-09-12 16:50:29 +0600

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Get the URL of a file in the CDN
+ * @param key - The key of the file in the CDN
+ * @param ext - The file extension
+ * @returns The URL of the file
+ */
 export function getFileUrl(key: string, ext?: string) {
   return `https://cdn.graphland.dev/${key}${ext ? `.${ext}` : ""}`;
 }
