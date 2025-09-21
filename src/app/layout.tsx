6414ab13f6abe31ae5ee91d57d289ece279c4cf0
kingrayhan <rayhan095@gmail.com>
2025-09-21 16:58:59 +0600

    Add favicon.ico to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,12 @@ export const metadata: Metadata = {
   title: "Graphland - Custom Solutions",
   description:
     "Custom solutions for websites, mobile apps, and desktop applications. Crafted with precision.",
+  openGraph: {
+    siteName: "Graphland",
+    phoneNumbers: ["+8801836980760"],
+    emails: ["contact@graphland.dev"],
+    images: [{ url: "/og.png" }],
+  },
 };
 
 export default function RootLayout({
@@ -23,11 +29,11 @@ export default function RootLayout({
         <meta name="color-scheme" content="dark" />
         <meta name="theme-color" content="#dff948" />
       </head>
-      <body className="antialiased cursor-none!">
+      <body className="antialiased">
         <Navigation />
         <main className="pt-16">{children}</main>
         <Footer />
-        <SmoothCursor />
+        {/* <SmoothCursor /> */}
       </body>
     </html>
   );
